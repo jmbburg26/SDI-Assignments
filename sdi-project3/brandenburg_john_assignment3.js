3fd2b26d20e48605545d7a98cf2de8f85a7dd47d
John Brandenburg <jmbburg26@fullsail.edu>
2012-12-06 19:24:59 -0500

    Added additional functions to the JavaScript file

--- sdi-project3/brandenburg_john_assignment3.js
+++ sdi-project3/brandenburg_john_assignment3.js
@@ -1,23 +1,56 @@
 //alert("JavaScript works!");var soldierName = "John";
 
 //My Global Variables
+var soldierName = "PFC Brandenburg";
+var	bestJobEver = "Cavalry Scout";
+var	goArmy = "Army";
+
+//My objects
 var	basicTraining = {
 		stationName:	"Fort Knox",
 		phases:			[ "Reception", "Red Phase", "Green Phase", "Blue Phase", "Gold Phase" ],
-		activities: 	[ "PT", "Shooting Range", "Rappelling Wall", "Bradley Driving Course", "20K Road March", "Graduation" ],
+		activities: 	[ "PT Test", "Shooting Range", "Rappelling Wall", "Bradley Driving Course", "20K Road March", "Graduation" ],
 		barricksName:	"Disney",
 		lotOfFun:		true,
 		getlocation:	function (location) {},
 		timeAtBasic:	function () {}
 };
-var soldierName = "PFC Brandenburg";
-var	bestJobEver = "Cavalry Scout";
-var	goArmy = "Army";
 
+var trainingBase = basicTraining.stationName;
+var favoriteActivities = basicTraining.activities;
+
+//My Boolean function
+var apftResults = function(totalScore){
+	if (totalScore >= 180) {
+		return "I successfully passed my APFT";
+	} else {
+		return "YOU NEED TO DO MORE PT!"};
+};
+var apftTestScore = apftResults(220);
+
+//My Number Function
+var roadMarch = function(totalDistance){
+var distanceConversion = 
+	totalDistance * .6;
+		return distanceConversion;
+};
+var roadMarchDistance = roadMarch(59);
 
 //Main Code
-var trainingBase = basicTraining.stationName;
-console.log("After spending all that time at " + trainingBase + ", " + soldierName + " is gonna tell us his favorite things about training.");
 
-var favoriteActivities = basicTraining.activities;
-console.log("The activities I enjoyed the most was " + favoriteActivities + ".");
+console.log("After spending all that time at " + trainingBase + ", " + soldierName + " is going to tell us his favorite things about training.");
+console.log("The activities he enjoyed the most were " + favoriteActivities + ".");
+console.log(soldierName + "'s least favorite was the " + favoriteActivities[0] + ".");
+console.log("With a score of 220, " + apftTestScore + "!");
+console.log("While training to be a " + bestJobEver + ", " + soldierName + " did a lot of Road Marches.");
+console.log("He did a total of 7 marches.");
+console.log("The total distance of the marches was " + roadMarchDistance + " miles.");
+console.log(roadMarchDistance);
+
+var goingHome = function(weeksOfBasic){
+ 	while (weeksOfBasic > 0){
+		weeksOfBasic--;
+		console.log(weeksOfBasic + " more weeks until I go home!");
+		}
+};
+var timeLeft = goingHome(20);
