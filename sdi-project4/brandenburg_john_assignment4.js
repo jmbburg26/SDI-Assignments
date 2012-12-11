c7a2cc1ec68038ce213bccf55aed110623a9b1c2
John Brandenburg <jmbburg26@fullsail.edu>
2012-12-11 08:50:48 -0500

    Added Convert String to Number function to script

--- sdi-project4/brandenburg_john_assignment4.js
+++ sdi-project4/brandenburg_john_assignment4.js
@@ -9,8 +9,31 @@
 
 var myCodeLibrary = function(){
 	
+	//Is String a URL?
 	
+	var isStringUrl = function(val){
+		if (val === "http:","https:"){
+			return true;
+		} else {
+			return false;
+		}
+	};
+	
+	//Convert a string to a number	
+	var convertToNumeric = function(val){
+		stringToNumber = parseInt(val);
+		return stringToNumber;
+		}
+	//Arrays
 
 
-
+	return {
+		"isStringUrl": 		isStringUrl,
+		"convertToNumeric":	convertToNumeric,
+	};
 };
+
+var newLib = myCodeLibrary();
+
+console.log("Is https://www.fullsail.edu an URL? " + newLib.isStringUrl("https://www.fullsail.edu"));
+console.log(newLib.convertToNumeric("365"));
