7b920b6d5b496e0a7e5e0d07219c743279c7957c
John Brandenburg <jmbburg26@fullsail.edu>
2012-12-13 00:38:50 -0500

    Added Check Email Function to the script

--- sdi-project4/brandenburg_john_assignment4.js
+++ sdi-project4/brandenburg_john_assignment4.js
@@ -8,32 +8,54 @@
 //My Library
 
 var myCodeLibrary = function(){
-	
+//Strings	
 	//Is String a URL?
 	
 	var isStringUrl = function(val){
 		if (val === "http:","https:"){
-			return true;
+			return "This is a valid URL";
 		} else {
-			return false;
+			return "This is NOT a valid URL";
 		}
 	};
-	
+
 	//Convert a string to a number	
 	var convertToNumeric = function(val){
 		stringToNumber = parseInt(val);
-		return stringToNumber;
-		}
+			return stringToNumber;
+		};
+
+	var findPhoneNumber = function(){
+		pattern = Pattern.compile("\\d{3}-\\d{3}-\\d{4}");
+		matcher = pattern.matcher(str);
+			if (matcher.find()) {
+    		System.out.println(matcher.group(0))
+    			return true;
+    		} else {return false;
+    	}
+	};
+	
+	function checkEmailPattern(emailToCheck) {
+    	var emailAddress = emailToCheck
+    	var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/
+    		if (emailPattern.test(emailAddress)) {
+        		return "This is a valid email address.";
+   				 } else {
+        		return "Not a valid email address."};
+	};
 	//Arrays
 
 
 	return {
 		"isStringUrl": 		isStringUrl,
 		"convertToNumeric":	convertToNumeric,
+		"checkEmailPattern":checkEmailPattern,
 	};
 };
 
 var newLib = myCodeLibrary();
 
+
 console.log("Is https://www.fullsail.edu an URL? " + newLib.isStringUrl("https://www.fullsail.edu"));
-console.log(newLib.convertToNumeric("365"));
+console.log(newLib.convertToNumeric("78"));
+console.log("Is jmbburg26@gmail.com a valid email address? " + newLib.checkEmailPattern("jmbburg26@gmail.com"));
